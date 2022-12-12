d8c551c52e3724931e882c77546250718df0dcec
Aviraj Khare <thisisavirajkhare@gmail.com>
2022-12-12 20:33:45 +0530

    [Aviraj] Added ink boxes

--- pages/ink-boxes.tsx
+++ pages/ink-boxes.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+import { Card, CardBody, CardTitle, CardText, Button } from 'reactstrap';
+
 export default function InkBoxes() {
     return (
         <div className={styles.container}>
@@ -14,12 +16,73 @@ export default function InkBoxes() {
                 <h1 className={styles.title}>
                     Welcome to Ink Boxes!
                 </h1>
+                <br />
                 <h2 className={styles.title}>
                     Live
                 </h2>
-                <p className={styles.description}>
-                    View our collection of Ink Boxes.
-                </p>
+                <Card
+                    style={{
+                        width: '18rem'
+                    }}
+                >
+                    <img
+                        alt="Flipper"
+                        src="https://i.imgur.com/EyZ1i2G.png"
+                    />
+                    <CardBody>
+                        <CardTitle tag="h5">
+                            Flipper
+                        </CardTitle>
+                        <CardText>
+                            This ink box contains flipper smart contract along with the UI.
+                        </CardText>
+                        <Button>
+                            Download
+                        </Button>
+                    </CardBody>
+                </Card>
+                <Card
+                    style={{
+                        width: '18rem'
+                    }}
+                >
+                    <img
+                        alt="ERC-20"
+                        src="https://i.imgur.com/y1V49R6.png"
+                    />
+                    <CardBody>
+                        <CardTitle tag="h5">
+                            ERC-20
+                        </CardTitle>
+                        <CardText>
+                            This ink box contains ERC-20 smart contract along with the UI.
+                        </CardText>
+                        <Button>
+                            Download
+                        </Button>
+                    </CardBody>
+                </Card>
+                <Card
+                    style={{
+                        width: '18rem'
+                    }}
+                >
+                    <img
+                        alt="ERC-721"
+                        src="https://i.imgur.com/YRyn4wh.png"
+                    />
+                    <CardBody>
+                        <CardTitle tag="h5">
+                            ERC-721
+                        </CardTitle>
+                        <CardText>
+                            This ink box contains ERC-721 smart contract along with the UI.
+                        </CardText>
+                        <Button>
+                            Download
+                        </Button>
+                    </CardBody>
+                </Card>
                 <h2 className={styles.title}>
                     Coming Soon
                 </h2>
