62d7f15fac46e52c77781f9be3c37876926a0f67
Aviraj Khare <thisisavirajkhare@gmail.com>
2022-12-12 08:14:59 +0530

    [Aviraj] Added hyperlink to /ink-boxes

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <p className={styles.description}>
           Ink boxes are a collection of helpful Ink smart contract boilerplates along with it&apos;s frontend. It will already have polkadot.js library using which frontend can talk with the smart contract deployed. Got inspired by truffle boxes on how easily one can spin up the boilerplate code in no time.
           <br />
-          Click here to explore Ink Boxes.
+          Click <a href='../ink-boxes'>here</a> to explore Ink Boxes.
         </p>
       </main>
     </div>
