bc7fd7f57acd8e0e0e1a439b469dc2cff21347fb
Aviraj Khare <thisisavirajkhare@gmail.com>
2022-12-28 16:05:04 +0530

    [Aviraj] Added image and style changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,18 +7,22 @@ import Header from '../components/Header'
 export default function Home() {
   return (
     <div>
-      <Header />
       <div className={styles.container}>
         <Head>
           <title>Ink Boxes</title>
           <meta name="description" content="Ink Boxes" />
+          <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
           <link rel="icon" href="/favicon.ico" />
         </Head>
-
+        <Header />
         <main className={styles.main}>
           <h1 className={styles.title}>
             Welcome to Ink Boxes!
           </h1>
+
+          <center>
+            <Image alt="Picture of boxes" src={'/boxes.svg'} width={400} height={400}></Image>
+          </center>
 
           <p className={styles.description}>
             Ink boxes are a collection of helpful Ink smart contract boilerplates along with it&apos;s frontend. It will already have polkadot.js library using which frontend can talk with the smart contract deployed. Got inspired by truffle boxes on how easily one can spin up the boilerplate code in no time.
